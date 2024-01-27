91ad3b86e223903f01a99f9d8c10b03863b6d0b0
cemil bahadir <cemil.enfal@lasoft.org>
2024-01-27 09:33:42 +0300

    calisiyor

--- src/main/java/com/example/monsterdevtelegram/MonsterdevtelegramApplication.java
+++ src/main/java/com/example/monsterdevtelegram/MonsterdevtelegramApplication.java
@@ -2,19 +2,17 @@ package com.example.monsterdevtelegram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.TelegramBotsApi;
-import org.telegram.telegrambots.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class MonsterdevtelegramApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MonsterdevtelegramApplication.class, args);
-
-        TelegramBotsApi botsApi = new TelegramBotsApi();
-
-        // Register our bot
         try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(new MyAmazingBot());
         } catch (TelegramApiException e) {
             e.printStackTrace();
--- src/main/java/com/example/monsterdevtelegram/MyAmazingBot.java
+++ src/main/java/com/example/monsterdevtelegram/MyAmazingBot.java
@@ -1,16 +1,12 @@
 package com.example.monsterdevtelegram;
 
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 @Component
 public class MyAmazingBot extends TelegramLongPollingBot {
-
-    @Override
-    public void onUpdateReceived(Update update) {
-        System.out.println("updatte "+update);
-    }
 
 
 
@@ -22,6 +18,18 @@ public class MyAmazingBot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return "6070612880:AAGZMXNMlPlQm8nYVBRQbmYQvHOWDSxYtvU";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        System.out.println("testtt"+update);
+    }
+
+
+
+    @Override
+    public void clearWebhook() throws TelegramApiRequestException {
+
     }
 }
 
